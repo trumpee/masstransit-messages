0413b30db37aa1b7127fddd9d6e676b941d44fda
Bardin08 <vbardin810@gmail.com>
2023-11-27 02:58:44 +0200

    Add value & value type to substitute values

--- Trumpee.MassTransit.Messages/Notifications/Variable.cs
+++ Trumpee.MassTransit.Messages/Notifications/Variable.cs
@@ -4,5 +4,7 @@ public class Variable
 {
     public required string Name { get; init; }
     public string? Description { get; init; }
+    public string? ValueType { get; init; }
+    public object? Value { get; init; }
     public string? Example { get; init; }
 }
