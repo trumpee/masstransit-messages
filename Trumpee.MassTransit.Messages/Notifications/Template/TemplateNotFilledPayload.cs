fe2e4c0a215c6c64ceaa8e1e4e8c27d2e7e49df0
Bardin08 <vbardin810@gmail.com>
2024-05-21 01:02:48 +0300

    Cherry-Pick analytical events structure

--- Trumpee.MassTransit.Messages/Notifications/Template/TemplateNotFilledPayload.cs
+++ Trumpee.MassTransit.Messages/Notifications/Template/TemplateNotFilledPayload.cs
@@ -4,4 +4,5 @@ public class TemplateNotFilledPayload
 {
     public required string TemplateId { get; set; }
     public required string NotificationId { get; set; }
+    public IReadOnlyList<string>? Errors { get; set; }
 }
---
+++ Trumpee.MassTransit.Messages/Notifications/Validation/Validation.cs
@@ -0,0 +1,29 @@
+﻿namespace Trumpee.MassTransit.Messages.Notifications.Validation;
+
+public static class Validation
+{
+    public static Event<ValidationFailedPayload> Failed(string source, string notificationId, string error)
+    {
+        return Event<ValidationFailedPayload>.Create(
+            notificationId,
+            "ValidationFailed",
+            source,
+            new ValidationFailedPayload
+            {
+                NotificationId = notificationId,
+                ErrorMessage = error
+            });
+    }
+
+    public static Event<ValidationPassedPayload> Passed(string source, string notificationId)
+    {
+        return Event<ValidationPassedPayload>.Create(
+            notificationId,
+            "ValidationPassed",
+            source,
+            new ValidationPassedPayload
+            {
+                NotificationId = notificationId,
+            });
+    }
+}
